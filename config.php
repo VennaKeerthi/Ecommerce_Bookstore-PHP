e7fd823109ef34871bfb1f8186367c60ed4ffa45
VennaKeerthi <vennakeerthi2004@gmail.com>
2024-12-25 12:51:53 +0530

    Modified

--- config.php
+++ config.php
@@ -1,6 +1,3 @@
-
-
-//$conn = mysqli_connect('sql303.infinityfree.com', 'if0_37850778', '5phAMU0VMdtIT', 'if0_37850778_test', 3306) or die('Connection failed: ' . mysqli_connect_error());
 <?php
 
 $host = getenv('DB_HOST');
