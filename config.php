8a91cee544add6fa25f1c6a32ce8ea454af116e2
VennaKeerthi <vennakeerthi2004@gmail.com>
2024-12-25 11:47:57 +0530

    Updated

--- config.php
+++ config.php
@@ -1,5 +1,15 @@
+
+
+//$conn = mysqli_connect('sql303.infinityfree.com', 'if0_37850778', '5phAMU0VMdtIT', 'if0_37850778_test', 3306) or die('Connection failed: ' . mysqli_connect_error());
 <?php
 
-$conn = mysqli_connect('sql303.infinityfree.com', 'if0_37850778', '5phAMU0VMdtIT', 'if0_37850778_test', 3306) or die('Connection failed: ' . mysqli_connect_error());
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
+$conn = mysqli_connect($host, $user, $password, $dbname, 3306) 
+    or die('Connection failed: ' . mysqli_connect_error());
 
 ?>
+
