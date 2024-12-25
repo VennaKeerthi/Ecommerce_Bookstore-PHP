eca948e3ff06303b9c59ec299e75f87e8044ac69
VennaKeerthi <vennakeerthi2004@gmail.com>
2024-12-25 12:37:27 +0530

    Modified

--- config.php
+++ config.php
@@ -7,9 +7,8 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$port = getenv('PORT');
 
-$conn = mysqli_connect($host, $user, $password, $dbname, $port) 
+$conn = mysqli_connect($host, $user, $password, $dbname) 
     or die('Connection failed: ' . mysqli_connect_error());
 
 ?>
