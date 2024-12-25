646dd63258dbbbc39d1f9f0e4d44af5af5948ab5
VennaKeerthi <vennakeerthi2004@gmail.com>
2024-12-25 11:43:03 +0530

    Updated

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','id22301616_root','Keerthi@11','id22301616_store') or die('connection failed');
+$conn = mysqli_connect('sql303.infinityfree.com', 'if0_37850778', '5phAMU0VMdtIT', 'if0_37850778_test', 3306) or die('Connection failed: ' . mysqli_connect_error());
 
 ?>
